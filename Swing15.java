7b239d8099b34c1aac4b77f0c6532f710d408959
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-10 22:41:55 +0530

    Add files via upload

---
+++ Swing15.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+
+// import javax.swing.JFrame;
+// import javax.swing.JOptionPane;
+
+
+
+
+public class Swing15 
+
+{
+       JFrame f;
+
+
+       Swing15()
+       {
+              f= new JFrame();
+
+              JOptionPane.showMessageDialog(f,"System Data Is Breached.","Alert",JOptionPane.WARNING_MESSAGE);
+
+              
+       }
+
+       public static void main(String[] args)
+       
+       {
+              
+              new Swing15();
+       }
+}
