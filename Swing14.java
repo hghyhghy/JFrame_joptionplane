8d42a39f10cdabf6b66f5123dc760812344b0413
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-10 22:29:50 +0530

    Add files via upload

---
+++ Swing14.java
@@ -0,0 +1,26 @@
+
+import javax.swing.*;
+
+//java option plane 
+
+
+public class Swing14
+ {
+         JFrame f;
+
+         Swing14()
+         {
+              f=  new JFrame();
+
+              JOptionPane.showMessageDialog(f, "Alert!,System is crashed.");
+
+
+         }
+
+          public static void main(String[] args) {
+              
+
+              new Swing14();
+
+          }
+}
