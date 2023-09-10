5226b24012e2d286ebc248bc69fb4f7e58e933b2
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-10 22:51:17 +0530

    Add files via upload

---
+++ Swing16.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+
+//importing java swing utility package 
+
+
+public class Swing16 
+
+{
+         //creating jframe 
+
+         JFrame f;
+      
+
+         Swing16()
+         {
+
+              f= new JFrame();
+
+              String name= JOptionPane.showInputDialog(f, "Enter System Name" );
+
+
+         }
+
+         public static void main(String[] args)
+         
+         {
+                new Swing16();
+         }
+
+}
